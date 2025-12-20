f1baaf1c186d4db49fca44d9c66aca3ab5cdbc57
douvewane <douvewane85@gmail.com>
2025-12-20 08:17:21 +0000

    Cours sur les Observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { CompteAddComponent } from './comptes/compte-add/compte-add.component';
 import { CompteListComponent } from './comptes/compte-list/compte-list.component';
+import { Observable, Subscriber, Subscription } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink,AsyncPipe,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
+  subscribers!:Subscription[];
+   textFormControl = new FormControl('');
+   text$?:Observable<string|null>;
+   hello$?:Observable<string>
+  ngOnInit(): void {
+    this.text$=this.textFormControl.valueChanges;
+    this.hello$=new Observable<string>((subscriber: Subscriber<string>)=>{
+        const letters="Hello Word";
+        for (let index = 0; index < letters.length; index++) {
+           setTimeout(()=>{
+           subscriber.next(letters[index]);
+           },(index+1)*1000)
+
+        }
+
+         setTimeout(()=>{
+           subscriber.error("Erreur de Test");
+           },5000)
+
+        setTimeout(()=>{
+           subscriber.complete();
+           },(letters.length)*1000)
+
+    })
+
+/*
+    const helloObserver1={
+      next:(value:string)=>{
+         console.log(`Ce caractere est ${value}`);
+         
+      }
+    }
+    this.subscribers?.push(this.hello$.subscribe({
+         next:(value:string)=>{
+         console.log(value);
+         
+        },
+         complete:()=>{
+          console.log("Fin de Reponse");
+        },
+        error:(err:string)=>{
+          console.log(err);
+        }
+      
+    }))
+    this.subscribers?.push(this.hello$.subscribe(helloObserver1))
+    */
+  }
   title = 'gest-compte';
+
 }
--- src/app/comptes/compte-list/compte-list.component.ts
+++ src/app/comptes/compte-list/compte-list.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { CompteService } from '../compte.service';
 import { DatePipe, NgClass } from '@angular/common';
 import { ComptePaginate } from '../../models/compte.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-compte-list',
@@ -11,22 +12,37 @@ import { ComptePaginate } from '../../models/compte.model';
   templateUrl: './compte-list.component.html',
   styleUrl: './compte-list.component.css'
 })
-export class CompteListComponent implements OnInit {
+export class CompteListComponent implements OnInit,OnDestroy {
  //comptes=this.compteService.getComptes();
    //dataPaginate:ComptePaginate|null=null;
     dataPaginate?:ComptePaginate // dataPaginate:ComptePaginate|undefined=undefined; 
 
    // dataPaginate!:ComptePaginate
    //injection de dépendance
+   private   subscription:Subscription|null=null;
   constructor(private compteService: CompteService,private route:ActivatedRoute) { 
 
   } 
+  ngOnDestroy(): void {
+       this.subscription?.unsubscribe()
+  }
   ngOnInit(): void {
-     this.route.queryParamMap.subscribe(qp=>{
+    /* this.route.queryParamMap.subscribe(qp=>{
        let page =Number(qp.get("page")||1)
         this.dataPaginate=this.compteService.getComptesPagines(page);
+     })*/
+     this.subscription=this.route.queryParamMap.subscribe({
+      next:(qp)=>{
+          let page =Number(qp.get("page")||1)
+          this.dataPaginate=this.compteService.getComptesPagines(page);
+      },
+      complete:()=>{
+
+      },
+      error:(err) =>{
+        
+      },
      })
-     
   }
 
 }
