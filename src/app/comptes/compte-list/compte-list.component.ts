b592d52706a15e82d275ec1dbeecdd680d061fc0
douvewane <douvewane85@gmail.com>
2025-12-20 17:28:37 +0000

    Demon-1

--- src/app/comptes/compte-list/compte-list.component.ts
+++ src/app/comptes/compte-list/compte-list.component.ts
@@ -1,25 +1,30 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { CompteService } from '../compte.service';
-import { DatePipe, NgClass } from '@angular/common';
+import { AsyncPipe, DatePipe, NgClass } from '@angular/common';
 import { ComptePaginate } from '../../models/compte.model';
-import { Subscription } from 'rxjs';
+import { combineLatest, debounceTime, map, Observable, startWith, Subscription, switchMap } from 'rxjs';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-compte-list',
   standalone: true,
-  imports: [RouterLink, DatePipe, NgClass],
+  imports: [RouterLink, DatePipe, NgClass,AsyncPipe,ReactiveFormsModule],
   templateUrl: './compte-list.component.html',
   styleUrl: './compte-list.component.css'
 })
 export class CompteListComponent implements OnInit,OnDestroy {
  //comptes=this.compteService.getComptes();
    //dataPaginate:ComptePaginate|null=null;
-    dataPaginate?:ComptePaginate // dataPaginate:ComptePaginate|undefined=undefined; 
+   // dataPaginate?:ComptePaginate // dataPaginate:ComptePaginate|undefined=undefined; 
 
    // dataPaginate!:ComptePaginate
    //injection de dépendance
-   private   subscription:Subscription|null=null;
+
+   //Data Paginate with Observable
+    dataPaginate$?:Observable<ComptePaginate>
+    private   subscription:Subscription|null=null;
+    seachFiltrerFormControl = new FormControl('');
   constructor(private compteService: CompteService,private route:ActivatedRoute) { 
 
   } 
@@ -31,18 +36,26 @@ export class CompteListComponent implements OnInit,OnDestroy {
        let page =Number(qp.get("page")||1)
         this.dataPaginate=this.compteService.getComptesPagines(page);
      })*/
-     this.subscription=this.route.queryParamMap.subscribe({
-      next:(qp)=>{
-          let page =Number(qp.get("page")||1)
-          this.dataPaginate=this.compteService.getComptesPagines(page);
-      },
-      complete:()=>{
+     const page$ = this.route.queryParamMap.pipe(
+         map(qp => Number(qp.get('page') || 1))
+    );
+     const search$ = this.seachFiltrerFormControl.valueChanges.pipe(
+       startWith(''),
+        map((value: string | null)=> value ? value : '' ),
+        debounceTime(500)
+      );
+        this.dataPaginate$ = combineLatest([page$, search$]).pipe(
+            switchMap(([page, search]) =>{
+              console.log(`${page}-${search}`);
+              
+                return  this.compteService.getComptesPaginesWithPaginate(search, page)
+            }
+          
+        )
+       );
 
-      },
-      error:(err) =>{
-        
-      },
-     })
   }
+
+   
 
 }
--- src/app/comptes/compte.service.ts
+++ src/app/comptes/compte.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { COMPTES } from '../../data/mock';
 import { Compte, ComptePaginate } from '../models/compte.model';
+import { delay, map, mergeMap, Observable, of, Subscriber, switchMap, timer } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -26,6 +27,68 @@ export class CompteService {
       totalPages:totalPages,
       pages:Array.from({length:totalPages},(_,i)=>i+1)
     };
+  }
+
+  getComptesPaginesWithPaginate(search:string,page:number=1, size:number=2):Observable<ComptePaginate>{
+    console.log(search);
+    
+    let filteredComptes=COMPTES;
+    if (search!="") {
+       filteredComptes=filteredComptes.filter(compte=>compte.mumero.includes(search));
+    }
+    
+    const startIndex=(page-1)*size;
+    const endIndex=startIndex+size;
+
+    const comptesPage=COMPTES.slice(startIndex,endIndex);
+    const totalComptes=COMPTES.length;
+    const totalPages=Math.ceil(totalComptes/size);
+   
+   /* return  new Observable((subscriber: Subscriber<ComptePaginate>)=>{
+        setTimeout(() => {
+            subscriber.next({
+            comptes:comptesPage,
+            currentPage:page,
+            size:size,
+            totalPages:totalPages,
+            pages:Array.from({length:totalPages},(_,i)=>i+1)
+           })
+        }, 5000);
+    })
+       
+    return of<ComptePaginate>({
+      comptes: comptesPage,
+      currentPage: page,
+      size: size,
+      totalPages: totalPages,
+      pages: Array.from({ length: totalPages }, (_, i) => i + 1)
+     }).pipe(
+     delay(5000)
+    );
+ 
+
+        return timer(5000).pipe(
+          map(() => ({
+            comptes: comptesPage,
+            currentPage: page,
+            size: size,
+            totalPages: totalPages,
+            pages: Array.from({ length: totalPages }, (_, i) => i + 1)
+          }))
+    );
+    */
+
+return timer(5000).pipe(
+   mergeMap(() =>
+     of({
+      comptes: comptesPage,
+      currentPage: page,
+      size: size,
+      totalPages: totalPages,
+      pages: Array.from({ length: totalPages }, (_, i) => i + 1)
+    })
+  )
+);
   }
 }
 
